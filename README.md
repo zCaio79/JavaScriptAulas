ad784113ace123200e29daea3493c701038125d0
zCaio79 <caiomendesfaria@gmail.com>
2024-05-08 14:32:05 -0300

    V01

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# **@octocatTópicos JavaScript :shipit:**
+
+> [!NOTE]
+> Aluno : ***Caio Henrique Faria Mendes***
+
+### **Conteúdos Abordados** :
+
+- [ IF , Else IF e Else ]
+- [ Switch Case ]
+- [ For loop ]
+- [ While loop ]
+
---
+++ topics.js
@@ -0,0 +1,34 @@
+
+// IF, ELSE IF, ELSE
+
+const idade = 15;
+
+if (idade == 18) {
+    console.log("Você completou 18 anos!!");
+}
+else if (idade < 18) {
+    console.log("Você é menor de idade!!");
+}
+else {
+    console.log("Você é maior de idade!!");
+}
+
+// SWITCH CASE
+
+const numero = 2;
+
+switch (numero) {
+    case 1:
+        console.log("Número 1");
+        break;
+    case 2:
+        console.log("Número 2");
+        break;
+    case 3:
+        console.log("Número 3");
+        break;
+    default:
+        console.log("Outro Número");
+        break;
+}
+
