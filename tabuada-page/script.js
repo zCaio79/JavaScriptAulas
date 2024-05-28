1246c565192fea7cb2a87e272409116c889d5560
Caio Mendes <129559720+zCaio79@users.noreply.github.com>
2024-05-28 15:31:34 -0300

    Comments added

--- tabuada-page/script.js
+++ tabuada-page/script.js
@@ -1,28 +1,45 @@
-const search = document.querySelector('#search')
-const warning = document.querySelector('.warning')
-search.addEventListener('click', () => {
-    const number = document.querySelector('#number').value
-    if (number > 0) {
-        
-        warning.classList.remove('active')
-        for (let i = (tabela.rows.length - 1); i > 0; i--) {
-            tabela.deleteRow(i);
-        }
+const search = document.querySelector('#search') // Seleciona o botão de busca
+const warning = document.querySelector('.warning') // Seleciona o elemento de aviso
 
-        for (i = 1; i < 11; i++) {
+// Adiciona evento de clique ao botão de busca
+search.addEventListener('click', () => { 
+    const number = document.querySelector('#number').value // Obtém o valor do input
+
+    // Verifica se o número é maior que zero
+    if (number > 0) { 
+
+        // Remove o aviso existente
+        warning.classList.remove('active') 
+
+        // Remove todas as linhas da tabela
+        for (let i = (tabela.rows.length - 1); i > 0; i--) { 
+            tabela.deleteRow(i)
+        }
+        
+        // Gera as linhas com cada conta de multiplicação e o seu resultado
+        for (i = 1; i < 11; i++) { 
+
             const tabela = document.querySelector('#tabela')
+
             linha = tabela.insertRow()
+            
             const conta = linha.insertCell()
             const resultado = linha.insertCell()
 
+            // uma coluna recebe a conta
+            conta.textContent = number + " x " + i + " = " 
+            
+            // a outra recebe o resultado
+            resultado.textContent = number * i 
+        }
+    } else {
 
-            conta.textContent = number + " x " + i + " = "
-            resultado.textContent = number * i
+        // Remove linhas da tabela
+        for (let i = (tabela.rows.length - 1); i > 0; i--) { 
+            tabela.deleteRow(i)
         }
-    }else{
-        for (let i = (tabela.rows.length - 1); i > 0; i--) {
-            tabela.deleteRow(i);
-        }
-        warning.classList.add('active')
+
+        // Da display em um aviso de Número inválido
+        warning.classList.add('active') 
     }
-});
+})
